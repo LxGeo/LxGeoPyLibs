8c5dedd7a2d36dec8465a4d16e784c1ed131bccb
sherif-med <cherif.abedrazek@gmail.com>
2022-06-01 18:35:16 +0200

    adding setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+    setup(
+    name='LxGeoPyLibs',
+    version='0.0.1',
+    author='sherif-med',
+    description='A package of common modules for geospatial processing',
+    long_description='A set modules utilities for processing and DL',
+    url='https://github.com/LxGeo/LxGeoPyLibs',
+    keywords='DL, datasets, formulas',
+    python_requires='>=3.7',
+    install_requires=[],
+    )
