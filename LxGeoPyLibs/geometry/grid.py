e1d88ced17cc259b7ccee2f3f265d1c7d7763836
sherif-med <cherif.abedrazek@gmail.com>
2022-08-09 19:02:56 +0200

    make_grid takes small window size on x&y axes || accepts pygeos geoms || accepts pygeos filter predicate

--- LxGeoPyLibs/geometry/grid.py
+++ LxGeoPyLibs/geometry/grid.py
@@ -1,8 +1,10 @@
+from cgitb import small
 import geopandas as gpd
-from shapely.geometry import Polygon, base
+from shapely.geometry import base
+import pygeos
 import numpy as np
 
-def make_grid(geom_container, x_step, y_step):
+def make_grid(geom_container, x_step, y_step, x_size, y_size, filter_predicate=lambda x:True):
     """
     Create a grid covering the extents of geometry container.
     Args:
@@ -12,20 +14,21 @@ def make_grid(geom_container, x_step, y_step):
         list of shapely polygons constructing the grid
     """
     if isinstance(geom_container, gpd.GeoDataFrame):
-        total_bounds = geom_container.total_bounds
+        geom_container = pygeos.box(*geom_container.total_bounds)
     elif isinstance(geom_container, base.BaseGeometry):
-        total_bounds = geom_container.bounds
+        geom_container = pygeos.from_shapely(geom_container)
+    elif isinstance(geom_container, pygeos.Geometry):
+        geom_container = geom_container
     else:
-        raise Exception("Cannot make grid for geometry container of type {}".format(type(geom_container)))
-    
-    xmin, ymin, xmax, ymax = total_bounds
-
-    cols = list(np.arange(xmin, xmax + x_step, x_step))
-    rows = list(np.arange(ymin, ymax + y_step, y_step))
-
+        raise Exception("Cannot make grid for geometry container of type {}".format(type(geom_container)))    
+    xmin, ymin, xmax, ymax = pygeos.bounds(geom_container)
+    cols = list(np.arange(xmin, xmax+x_step, x_step))
+    rows = list(np.arange(ymin, ymax+y_step, y_step))
     polygons = []
     for x in cols[:-1]:
         for y in rows[:-1]:
-            polygons.append(Polygon([(x,y), (x+x_step, y), (x+x_step, y+y_step), (x, y+y_step)]))
-    
+            small_window = pygeos.linearrings([(x,y), (x+x_size, y), (x+x_size, y+y_size), (x, y+y_size)])
+            if filter_predicate(small_window):
+                polygons.append(small_window)    
+    polygons = pygeos.polygons(polygons)
     return polygons
