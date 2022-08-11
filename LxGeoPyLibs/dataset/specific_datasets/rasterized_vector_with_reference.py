2cfd5b97135491769a69586871b0f9205ba655ec
sherif-med <cherif.abedrazek@gmail.com>
2022-08-11 18:45:12 +0200

    extending rasterized_vec_with_ref from patchified_dataset instead raster_dataset && usage of datasets composition instead of inheritance && adding rasterization_method option

--- LxGeoPyLibs/dataset/specific_datasets/rasterized_vector_with_reference.py
+++ LxGeoPyLibs/dataset/specific_datasets/rasterized_vector_with_reference.py
@@ -1,28 +1,43 @@
-from inspect import Attribute
-from LxGeoPyLibs.dataset.raster_dataset import RasterDataset, rasters_map
+from LxGeoPyLibs.dataset.raster_dataset import RasterDataset
+from LxGeoPyLibs.dataset.vector_dataset import VectorDataset
+from LxGeoPyLibs.dataset.patchified_dataset import PatchifiedDataset
+from torch.utils.data import Dataset
 from LxGeoPyLibs.geometry.utils_pygeos import get_pygeos_geom_creator, get_pygeos_transformer
 import fiona
 import pyproj
 import pygeos
+import torch
+from LxGeoPyLibs.vision.image_transformation import Trans_Identity
+from LxGeoPyLibs.geometry.rasterizers.polygons_rasterizer import polygons_to_multiclass
 
-class VectorWithRefDataset(RasterDataset):
+class VectorWithRefDataset(Dataset, PatchifiedDataset):
     """
     Dataset used to load vector and raster data.
     Example of use case: load non aligned vector map with respective reference map as a raster of probability.
     """
 
-    def __init__(self, image_path:str, vector_path:str, force_coord_transform=False, **kwargs):
+    def __init__(self, image_path:str, vector_path:str, rasterization_method, force_coord_transform=False,
+     augmentation_transforms=None,preprocessing=None, bounds_geom=None, 
+     patch_size=None, patch_overlap=None):
         """
         Args:
             -image_path: filepath of raster map file (tif)
             -vector_path: filepath of vector map file (shp, TAB)
+            -rasterization_method: Callable that transforms vectors to rasters
             -force_coord_transform: if True transforms geometries to raster crs if different
         """
-        RasterDataset.__init__(self, image_path=image_path, **kwargs)
-        self.vector_dataset = fiona.open(vector_path)
+        self.image_dataset = RasterDataset(image_path=image_path)
+        self.vector_dataset = VectorDataset(vector_path=vector_path)
+
+        self.rasterization_method = rasterization_method
+        if augmentation_transforms is None:
+            self.augmentation_transforms=[Trans_Identity()]
+        else:
+            self.augmentation_transforms = augmentation_transforms
+        self.preprocessing=preprocessing
         
         # check crs correspondance
-        crs_are_equal = self.vector_dataset.crs == rasters_map[self.image_path].crs
+        crs_are_equal = self.image_dataset.rio_dataset().crs == self.vector_dataset.fio_dataset().crs
         if not crs_are_equal:
             if not force_coord_transform:
                 print("Vector and raster inputs don't share the same crs!")
@@ -30,51 +45,82 @@ class VectorWithRefDataset(RasterDataset):
                 raise Exception("CRS mismatch.")
             
         projection_transformer = pyproj.Transformer.from_proj(
-            pyproj.Proj(init=self.vector_dataset.crs["init"]),
-            pyproj.Proj(init=rasters_map[self.image_path].crs)
+            pyproj.Proj(init=self.vector_dataset.fio_dataset().crs["init"]),
+            pyproj.Proj(init=self.image_dataset.rio_dataset().crs)
         )
         pygeos_transformer = get_pygeos_transformer(projection_transformer)
 
-        vector_bounds_geom = pygeos_transformer(pygeos.box(*self.vector_dataset.bounds))
-        raster_bounds_geom = pygeos.box(*rasters_map[self.image_path].bounds)
+        vector_bounds_geom = pygeos_transformer(pygeos.box(*self.vector_dataset.fio_dataset().bounds))
+        raster_bounds_geom = pygeos.box(*self.image_dataset.rio_dataset().bounds)
         common_area_geom = pygeos.intersection(vector_bounds_geom,raster_bounds_geom)
         if pygeos.is_empty(common_area_geom):
             print("Vector and raster don't have common area!")
             raise Exception("Area mismatch.")
         
-        patch_size=kwargs.get("patch_size")
-        patch_overlap=kwargs.get("patch_overlap")
+        if bounds_geom:
+            common_area_geom = pygeos.intersection(common_area_geom, bounds_geom)
+        
         if not None in (patch_size, patch_overlap):
             self.setup_spatial(patch_size, patch_overlap, common_area_geom)
-    
-    
-    def vector_geometry_type(self):
-        return self.vector_dataset.meta["schema"]["geometry"]
-
-    def _load_vector_geometries_window(self, window_geom, crop=True):
-        """
-        Function to load geometries from vector within a window
-        """
-
-        features_coords = []
-        for c_feature in self.vector_dataset.filter(bbox=pygeos.bounds(window_geom).tolist()):
-            features_coords.append( c_feature["geometry"]["coordinates"] )
         
-        pygeos_geom_creator = get_pygeos_geom_creator(self.vector_geometry_type())
+    def setup_spatial(self, patch_size, patch_overlap, bounds_geom=None):
+        """
+        Setup patch loading settings using spatial coordinates.
+        Args:
+            patch_size: a tuple of positive integers in pixels.
+            patch_overlap: a positive integer in pixels.
+            bounds_geom: pygeos polygon
+        """
+        self.patch_size= patch_size
+        self.patch_overlap= patch_overlap
+        
+        # If no bounds provided, use image bounds
+        if not bounds_geom:
+            bounds_geom = self.bounds_geom
 
-        geometries = pygeos_geom_creator(features_coords)
+        pixel_x_size = self.image_dataset.rio_dataset().transform[0]
+        pixel_y_size = -self.image_dataset.rio_dataset().transform[4]
 
-        if crop: geometries=pygeos.intersection(geometries, window_geom)
+        patch_size_spatial = (self.patch_size[0]*pixel_x_size, self.patch_size[1]*pixel_y_size)
+        patch_overlap_spatial = self.patch_overlap*pixel_x_size
 
-        return geometries
+        super(Dataset, self).__init__(patch_size_spatial, patch_overlap_spatial, bounds_geom)
+    
+    def __getitem__(self, idx):
+        
+        assert self.is_setup, "Dataset is not set up!"
+        window_idx = idx // (len(self.augmentation_transforms))
+        transform_idx = idx % (len(self.augmentation_transforms))
+        
+        window_geom = super(Dataset, self).__getitem__(window_idx)
+        
+        img = self.image_dataset._load_padded_raster_window(window_geom, self.patch_size)
+        vec = self.vector_dataset._load_vector_geometries_window(window_geom)
+        # rasterio accepts only features that implements __geo_interface__
+        vec = map(lambda x: pygeos.to_shapely(x), vec)
+        rasterized_vec = self.rasterization_method(vec, window_geom, gsd=self.image_dataset.gsd(), crs=self.image_dataset.rio_dataset().crs)
+        
+        c_trans = self.augmentation_transforms[transform_idx]
+        img, rasterized_vec = c_trans(img, rasterized_vec)
+        
+        if self.preprocessing:
+            img = self.preprocessing(img)
+        
+        img = torch.from_numpy(img).float()
+        rasterized_vec = torch.from_numpy(rasterized_vec).float()
+        return img, rasterized_vec
+    
+    
+    
 
 if __name__ == "__main__":
 
     in_raster = "../DATA_SANDBOX/lxFlowAlign/data/train_data/paris_ortho1_rooftop/flow.tif"
     in_vector = "../DATA_SANDBOX/lxFlowAlign/data/raw/paris/DL/ortho1/rooftop/buildings.shp"
-    c_r = VectorWithRefDataset(in_raster,in_vector, patch_size=(256,256), patch_overlap=0 )
+    rasterization_method = polygons_to_multiclass
+    c_r = VectorWithRefDataset(in_raster,in_vector, rasterization_method=rasterization_method, patch_size=(256,256), patch_overlap=0 )
 
-    a=c_r._load_vector_geometries_window(c_r.patch_grid[20])
+    a=c_r[20]
     print(a)
     pass
 
