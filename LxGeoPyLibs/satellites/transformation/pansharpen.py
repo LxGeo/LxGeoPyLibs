7c4adfaefa04cd598879c85209bffeb90edd5ef4
sherif-med <cherif.abedrazek@gmail.com>
2022-10-06 11:58:24 +0200

    fixing pansharpening bands count & support older gdal version pansharpening script

--- LxGeoPyLibs/satellites/transformation/pansharpen.py
+++ LxGeoPyLibs/satellites/transformation/pansharpen.py
@@ -1,4 +1,4 @@
-
+import inspect
 from osgeo_utils.gdal_pansharpen import gdal_pansharpen
 import rasterio as rio
 
@@ -13,12 +13,18 @@ def pansharpen_image(input_pan_path, input_ms_path, out_path, band_nums=None):
 
     if band_nums is None:
         with rio.open(input_ms_path) as dst:
-            band_nums = list(range(dst.profile["count"]+1))
+            band_nums = list(range(1, dst.profile["count"]+1))
     
-    output_code = gdal_pansharpen(
-    pan_name=input_pan_path,    
-    spectral_names=[input_ms_path], 
-    band_nums=[1, 2, 3],
-    dst_filename=out_path)
+    
+    
+    if "pan_name" in inspect.getargspec(gdal_pansharpen).args:
+        output_code = gdal_pansharpen(
+        pan_name=input_pan_path,    
+        spectral_names=[input_ms_path], 
+        band_nums=band_nums,
+        dst_filename=out_path)
+    else:
+        command_args = [""] + [str(item) for sublist in zip(["-b"]*len(band_nums), band_nums) for item in sublist] + [input_pan_path, input_ms_path, out_path ]
+        output_code = gdal_pansharpen(command_args)
 
     return output_code
