ed2911e0b4f08b9cbf13c317def93360b29629a5
sherif-med <cherif.abedrazek@gmail.com>
2022-12-09 18:41:27 +0100

    updating Image_Transformation to to accept variable amount of input images && adding __fwd__ and __rwd__ for each transform && using np rot90 for spatial roations

--- LxGeoPyLibs/vision/image_transformation.py
+++ LxGeoPyLibs/vision/image_transformation.py
@@ -8,72 +8,103 @@ Created on Mon Mar 14 18:50:07 2022
 from skimage.transform import rotate
 from skimage import exposure
 import numpy as np
+import torch
+
+rot90_fn = lambda x, times: np.rot90(x, k=times, axes=(1, 2))
 
 class Image_Transformation:
     
-    def __call__(image,gt, *args):
-        pass
+    def __call__(self, *args, **kwargs):
+        
+        to_return = []
+        for c_arg in args:
+            if isinstance(c_arg, (np.ndarray,torch.Tensor)):
+                if kwargs.get("reverse", False):
+                    to_return.append(self.__rwd__(c_arg)[0])
+                else:
+                    to_return.append(self.__fwd__(c_arg)[0])
+            else:
+                to_return.append( self.__call__(*c_arg) )
+        return to_return
+        """
+        if kwargs.get("reverse", False):
+            return self.__rwd__(*args)
+        else:
+            return self.__fwd__(*args)"""
+    
+    def __fwd__(self, *args):
+        raise NotImplementedError
+    
+    def __rwd__(self, *args):
+        raise NotImplementedError
+        
 
 class Trans_Identity(Image_Transformation):
-    def __call__(self, image1, gt1, *args):        
-        return image1, gt1, args
+    
+    def __fwd__(self, *args):
+        return args
+    def __rwd__(self, *args):
+        return args
     
 class Trans_Rot90(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
-        image1_t = rotate(image1, 90)
-        gt1_t = rotate(gt1, 90)
-        
-        return (image1_t, gt1_t, *(rotate(c_arg,90) for c_arg in args))
+    
+    def __fwd__(self, *args):
+        return tuple(rot90_fn(c_arg,1) for c_arg in args)
+    def __rwd__(self, *args):
+        return Trans_Rot270()(*args)
+    
 
 class Trans_Rot180(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
-        image1_t = rotate(image1, 180)
-        gt1_t = rotate(gt1, 180)
-        
-        return (image1_t, gt1_t, *(rotate(c_arg,180) for c_arg in args))
+    
+    def __fwd__(self, *args):
+        return tuple(rot90_fn(c_arg,2) for c_arg in args)
+    def __rwd__(self, *args):
+        return self.__fwd__(*args)
 
 class Trans_Rot270(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
-        image1_t = rotate(image1, 270)
-        gt1_t = rotate(gt1, 270)
+    
+    def __fwd__(self, *args):
+        return tuple(rot90_fn(c_arg,3) for c_arg in args)
+    def __rwd__(self, *args):
+        return Trans_Rot90()(*args)
         
-        return (image1_t, gt1_t, *(rotate(c_arg,270) for c_arg in args))
 
 class Trans_Flipud(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
-        image1_t = np.flipud(image1)
-        gt1_t = np.flipud(gt1)
-        
-        return (image1_t, gt1_t, *(np.flipud(c_arg) for c_arg in args))
+    
+    def __fwd__(self, *args):
+        return tuple(np.flipud(c_arg) for c_arg in args)
+    def __rwd__(self, *args):
+        return self.__fwd__(args)
+    
     
 class Trans_fliplr(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
-        image1_t = np.fliplr(image1)
-        gt1_t = np.fliplr(gt1)
-        
-        return (image1_t, gt1_t, *(np.fliplr(c_arg) for c_arg in args))
-
+    
+    def __fwd__(self, *args):
+        return tuple(np.fliplr(c_arg) for c_arg in args)
+    def __rwd__(self, *args):
+        return self.__fwd__(args)
+    
 class Trans_gaussian_noise(Image_Transformation):
-    def __call__(self, image1, gt1, *args):
+    def __call__(self, image1, gt1):
         noise = np.random.normal(loc = 0.0, scale = 2, size = image1.shape) 
         image1_t = np.clip(image1+noise, 0 ,255).astype(image1.dtype)
-        return (image1_t, gt1, args)
+        return (image1_t, gt1)
 
 class Trans_gamma_adjust(Image_Transformation):    
     def __init__(self, gamma=1.5):
         self.gamma = gamma
         
-    def __call__(self, image1, gt1, *args):
+    def __call__(self, image1, gt1):
         image1_t = exposure.adjust_gamma(image1/255, self.gamma)*255        
-        return (image1_t, gt1, args)
+        return (image1_t, gt1)
 
 class Trans_equal_hist(Image_Transformation):    
-    def __call__(self, image1, gt1, *args):
+    def __call__(self, image1, gt1):
         image1_t = (exposure.equalize_hist(image1)*255).astype(image1.dtype)
-        return (image1_t, gt1, args)
+        return (image1_t, gt1)
 
 class Trans_contrast_stretch(Image_Transformation):    
-    def __call__(self, image1, gt1, *args):
+    def __call__(self, image1, gt1):
         p2, p98 = np.percentile(image1, (2, 98))
         image1_t = exposure.rescale_intensity(image1, in_range=(p2, p98))*255
-        return (image1_t, gt1, args)
+        return (image1_t, gt1)
