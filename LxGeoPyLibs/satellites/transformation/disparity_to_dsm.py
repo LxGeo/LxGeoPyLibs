adding new roof2roof formula && updating epipolar and dsm generation to use couple_file instead of explicit rotation and refinement values && fixing rotation fusion function

LxGeoPyLibs/satellites/transformation/disparity_to_dsm.py
@@ -1,10 +1,13 @@
 from LxGeoPyLibs.dataset.large_raster_utils import load_vips, vips2numpy
 import pyvips
 import click
+import json
+import affine
 import rasterio as rio
+import numpy as np
 from rasterio.plot import reshape_as_raster
 
-def disparity_to_dsm(disparity_map, inv_rotation_angle, v_displacement, out_dsm_path, profile, scale_factor=1/10 ):
+def disparity_to_dsm(disparity_map, inv_rotation_angle, v_displacement, out_dsm_path, profile, scale_factor ):
     """
     Apply reverse rotation of disparity map over respective image (epipolar 1 or 2)
     Args:
@@ -29,27 +32,40 @@ def disparity_to_dsm(disparity_map, inv_rotation_angle, v_displacement, out_dsm_
     start_h = (disparity_map_reversed.height//2 - profile['height']//2 )
     start_w = (disparity_map_reversed.width//2 - profile['width']//2 )    
     disparity_map_reversed = disparity_map_reversed.crop(start_w, start_h, profile['width'], profile['height'])
-    profile["dtype"] = rio.float32; profile["nodata"]=None; profile["count"]=1 
+    profile["dtype"] = rio.float32; profile["nodata"]=-32768*scale_factor; profile["count"]=1 
     with rio.open(out_dsm_path, "w", **profile) as tar:
         tar.write(reshape_as_raster(vips2numpy(disparity_map_reversed)))
 
 
 @click.command()
 @click.option('-idisp', '--input_disparity', required=True, type=click.Path(exists=True), help="Path to input disparity map")
-@click.option('-ro', '--ref_ortho', required=True, type=click.Path(exists=True), help="Path to original ortho image (not epipolar)")
-@click.option('-ira', '--inverse_rotation_angle', required=True, type=click.FLOAT, help="Rotation angle to use to inverse epipolarity")
-@click.option('-rvd', '--refinement_vertical_disp', required=True, type=click.INT, help="Vertical displacement used to refine epipolar")
+#@click.option('-ro', '--ref_ortho', required=True, type=click.Path(exists=True), help="Path to original ortho image (not epipolar)")
+#@click.option('-ira', '--inverse_rotation_angle', required=True, type=click.FLOAT, help="Rotation angle to use to inverse epipolarity")
+#@click.option('-rvd', '--refinement_vertical_disp', required=True, type=click.FLOAT, help="Vertical displacement used to refine epipolar")
 @click.option('-o', '--output_path', required=True, type=click.Path(), help="Path to output dsm map")
+@click.option('-c_f', '--couple_file', required=True, type=click.Path(exists=True), help="Couple file too be used to reverese disparity")
 @click.option('-sf', '--scale_factor', default=1/10, type=click.FLOAT, help="Scale factor used to turn disp into meters")
-def main(input_disparity, ref_ortho, inverse_rotation_angle, refinement_vertical_disp, output_path, scale_factor):
+def main(input_disparity, output_path, couple_file, scale_factor):
     
-    with rio.open(ref_ortho) as dst:
-        out_dsm_profile = dst.profile.copy()
-        out_dsm_profile.update({"count":1, "dtype":rio.float32})
+    with open(couple_file) as f:
+        details_dict=json.load(f)
+
+    out_dsm_profile = {
+        "width":details_dict["ref1_width"],
+        "height":details_dict["ref1_height"],
+        "crs":details_dict["crs"],
+        "transform": affine.Affine.from_gdal( *details_dict["ref1_transform"]),
+        "count":1, "dtype":rio.float32
+    }
+
     # disparity scale factor: (1/10) output disparity are in tenth of pixels & resolution is mandatory to get heights in meter
-    disparity_scale_factor = 1/10; disparity_scale_factor /= out_dsm_profile["transform"].a
+    disparity_scale_factor = scale_factor
+    disparity_scale_factor *= out_dsm_profile["transform"].a
+    disparity_scale_factor /= np.linalg.norm(details_dict["xy_cst"])
     
-    disparity_to_dsm(input_disparity, inverse_rotation_angle, refinement_vertical_disp, output_path, out_dsm_profile, scale_factor)
+    inverse_rotation_angle = -details_dict["rotation_angle"]
+    refinement_vertical_disp = -details_dict["v_disp"]
+    disparity_to_dsm(input_disparity, inverse_rotation_angle, refinement_vertical_disp, output_path, out_dsm_profile, disparity_scale_factor)
     
 
 if __name__ == "__main__":

LxGeoPyLibs/satellites/transformation/epipolar_gen.py
@@ -1,8 +1,8 @@
 import click
-import pyvips
+import json
 from LxGeoPyLibs import _logger
 from LxGeoPyLibs.satellites.imd import IMetaData
-from LxGeoPyLibs.satellites.formulas import compute_rotation_angle
+from LxGeoPyLibs.satellites.formulas import compute_rotation_angle, compute_roof2roof_constants
 import math
 from LxGeoPyLibs.dataset.large_raster_utils import load_vips, vips2numpy, crop_image
 import cv2
@@ -11,15 +11,32 @@ from scipy.stats import gaussian_kde
 import rasterio as rio
 
 
-def lazy_ref_epipolar_creation(iref1, iref2, i_alt1, i_alt2, epi1, epi2, rotation_angle):
+def lazy_ref_epipolar_creation(iref1, input_imd_1, iref2, input_imd_2, i_alt1, i_alt2, epi1, epi2, couple_file):
     """
     Creates refined epipolar images using feature descriptor matching.
     Returns displacment applied vertically to refine epipolar/
     """
+
+    if input_imd_1 and input_imd_2:
+        imd1_obj = IMetaData(input_imd_1)
+        imd2_obj = IMetaData(input_imd_2)
+        rotation_angle = math.degrees(compute_rotation_angle(
+         math.radians(imd1_obj.satAzimuth()),
+         math.radians(imd1_obj.satElevation()),
+         math.radians(imd2_obj.satAzimuth()),
+         math.radians(imd2_obj.satElevation()))
+         )
+    else:
+        _logger.warning("None or missing IMD data provded!")
+        _logger.warning("Estimating epipolarity angle using close feature displacement!")
+        rotation_angle,_,_ = estimate_epipolarity_properties(iref1=iref1, iref2=iref2)
+    
+    _logger.info(f"Epipolarity angle = {rotation_angle}")
     
     with rio.open(iref1) as dst:
         ref1_x_size = dst.transform[0]
         ref1_bounds = dst.bounds
+        ref1_profile = dst.profile.copy()
 
     with rio.open(iref2) as dst:
         ref2_x_size = dst.transform[0]
@@ -59,7 +76,27 @@ def lazy_ref_epipolar_creation(iref1, iref2, i_alt1, i_alt2, epi1, epi2, rotatio
     i_alt1_epi1.write_to_file(epi1)
     i_alt1_epi2.write_to_file(epi2)
     
-    return v_disp
+    _logger.info(f"Vertical displacment = {v_disp}")
+
+    xy_cst = compute_roof2roof_constants(math.radians(imd1_obj.satAzimuth()),
+         math.radians(imd1_obj.satElevation()),
+         math.radians(imd2_obj.satAzimuth()),
+         math.radians(imd2_obj.satElevation())
+        )
+
+    detail_dict = {
+        "ref1_transform": ref1_profile["transform"].to_gdal(),
+        "ref1_width":ref1_profile["width"],
+        "ref1_height":ref1_profile["height"],
+        "crs":str(ref1_profile["crs"]),
+        "rotation_angle": rotation_angle,
+        "v_disp": v_disp,
+        "xy_cst": xy_cst
+    }
+    with open(couple_file, "w") as f:
+        json.dump(detail_dict, f)
+    
+    return
 
 def estimate_2d_displacement(im_array_1, im_array_2):
     """
@@ -146,29 +183,20 @@ def estimate_epipolarity_properties( iref1, iref2 ):
  )
 @click.option('-epi1', '--output_epi_image_1', required=True, type=click.Path(), help="First epipolar image output path")
 @click.option('-epi2', '--output_epi_image_2', required=True, type=click.Path(), help="Second epipolar image output path")
-def main(input_reference_image_1, input_imd_1, input_reference_image_2, input_imd_2, input_alternative_image_1, input_alternative_image_2, output_epi_image_1, output_epi_image_2):
+@click.option('-c_f', '--couple_file', required=True, type=click.Path(), help="Couple file too be used to reverese disparity")
+def main(input_reference_image_1, input_imd_1, input_reference_image_2, input_imd_2,
+ input_alternative_image_1, input_alternative_image_2,
+  output_epi_image_1, output_epi_image_2, couple_file):
     """
     A program to generate epipolar images using input_reference_images and respective IMD (optional).
     It can create epipolar out of alterantive input images using the option 'input_alternative_image_<i>' but the following requierments (Same size, Same geotransform, Same projection)
     """
     
-    if input_imd_1 and input_imd_2:
-        imd1_obj = IMetaData(input_imd_1)
-        imd2_obj = IMetaData(input_imd_2)
-        epipolarity_angle = math.degrees(compute_rotation_angle(imd1_obj.satAzimuth(), imd1_obj.satElevation(), imd2_obj.satAzimuth(), imd2_obj.satElevation()))
-    else:
-        _logger.warning("None or missing IMD data provded!")
-        _logger.warning("Estimating epipolarity angle using close feature displacement!")
-        epipolarity_angle,_,_ = estimate_epipolarity_properties(iref1=input_reference_image_1, iref2=input_reference_image_2)
-    
-    _logger.info(f"Epipolarity angle = {epipolarity_angle}")
-
     if not input_alternative_image_1: input_alternative_image_1=input_reference_image_1
     if not input_alternative_image_2: input_alternative_image_2=input_reference_image_2
-    lazy_ref_epipolar_creation(input_reference_image_1, input_reference_image_2,
+    lazy_ref_epipolar_creation(input_reference_image_1, input_imd_1, input_reference_image_2, input_imd_2,
      input_alternative_image_1, input_alternative_image_2,
-      output_epi_image_1, output_epi_image_2,
-       epipolarity_angle
+      output_epi_image_1, output_epi_image_2, couple_file
        )
 
 

LxGeoPyLibs/satellites/transformation/rotations_fusion.py
@@ -33,7 +33,7 @@ def aggregate_arrays(stacked: np.ndarray, method: aggregationMethod, nodata=None
     """
     Aggregates input arrays using method along axis
     """
-    stacked = stacked.astype("float"); stacked[stacked==nodata]=np.nan
+    #stacked = stacked.astype("float"); stacked[stacked==nodata]=np.nan
     if method == aggregationMethod.max:
         return reversed_bands_cummulation(stacked.max(axis))
     if method == aggregationMethod.min:
@@ -62,7 +62,7 @@ def get_stacked_view(datasets_map, st_row, st_col, tile_size):
     rotated_views_list = []
     for k,v in datasets_map.items():
         loaded_array = v.read(window=Window(st_row, st_col, tile_size, tile_size)).astype('float')
-        loaded_array[loaded_array==v.profile["nodata"]] = np.nan
+        #loaded_array[loaded_array==v.profile["nodata"]] = np.nan
         rotated_views_list.append(loaded_array)
     return np.stack(rotated_views_list)
 