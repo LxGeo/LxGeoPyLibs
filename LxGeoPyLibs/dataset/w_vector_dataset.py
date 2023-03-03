78f36142c13d2b9f9b1d5eba432dd1b905226f27
unknown <macherif@luxcarta.com>
2023-03-03 15:33:08 +0100

    removing opend fiona dataset from w_vector && droping id field at save time

--- LxGeoPyLibs/dataset/w_vector_dataset.py
+++ LxGeoPyLibs/dataset/w_vector_dataset.py
@@ -9,52 +9,29 @@ class WriteMode(Enum):
     overwrite = 1
     append = 2
 
-def get_layer_feature_ids(vector_path):
-    """
-    Returns all features ids in a layer as a set
-    """
-    ids_set = set()
-    for c_feature in fiona.open(vector_path):        
-        ids_set.update(c_feature["id"])
-    return ids_set
 
-class WVectorDataset():
+class WVectorDataset(object):
 
-    def __init__(self, vector_path:str, mode=WriteMode.new, driver=None, schema=None, crs=None):
+    def __init__(self, vector_path:str, mode=WriteMode.new, driver="ESRI Shapefile", schema=None, crs=None):
 
         assert (mode != WriteMode.new) or not os.path.exists(vector_path), "WVectorDataset with mode 'new' for existing path !"
         self.vector_path = vector_path
-
-        if (mode == WriteMode.append):
-            open_mode = "a"
-        else:
-            open_mode = "w"
-            driver = "ESRI Shapefile"
         
-        self.dataset = fiona.open(vector_path, open_mode, driver=driver, schema=schema, crs=crs)
+        self.saved_ids = set()
 
         if mode in (WriteMode.new, WriteMode.overwrite):
-            self.saved_ids = set()
             self.c_write_mode="w"
         else:
             self.c_write_mode="a"
-            self.saved_ids = get_layer_feature_ids(self.vector_path)
-    
-
-    def get_layer_feature_ids(self):
-        """
-        Returns all features ids in a layer as a set
-        """
-        ids_set = set()
-        for c_feature in self.dataset:        
-            ids_set.update(c_feature["id"])
-        return ids_set
-    
+            with fiona.open(vector_path) as dataset:                
+                for c_feature in dataset:        
+                    self.saved_ids.update(c_feature["id"])
+        
     def add_feature(self, features_gdf):
         """
         """
         features_gdf.drop(features_gdf[features_gdf["id"].isin(self.saved_ids)].index, inplace=True)
         if not features_gdf.empty:
             self.saved_ids.update(features_gdf["id"].values)
-            features_gdf.to_file(self.vector_path, mode=self.c_write_mode)
+            features_gdf.drop('id', axis=1).to_file(self.vector_path, mode=self.c_write_mode)
             self.c_write_mode="a"
