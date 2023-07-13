0ac6104097e7b437b03c1d54b485e3ae679a729f
sherif-med <cherif.abedrazek@gmail.com>
2023-07-13 21:50:26 +0200

    adding rasterDataset to registery

--- LxGeoPyLibs/dataset/__init__.py
+++ LxGeoPyLibs/dataset/__init__.py
@@ -2,10 +2,12 @@
 
 from LxGeoPyLibs.dataset.specific_datasets.raster_with_references import RasterWithRefsDataset
 from LxGeoPyLibs.dataset.specific_datasets.rasterized_vector_with_reference import VectorWithRefDataset
+from LxGeoPyLibs.dataset.raster_dataset import RasterDataset
 
 DATASETS_REGISTERY = {
     
     "RasterWithRefsDataset": RasterWithRefsDataset,
-    "VectorWithRefDataset": VectorWithRefDataset
+    "VectorWithRefDataset": VectorWithRefDataset,
+    "RasterDataset": RasterDataset
     
 }
