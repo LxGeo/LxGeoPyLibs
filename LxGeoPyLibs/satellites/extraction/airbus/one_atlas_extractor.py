da135ca8155b48aca5e0fbcd2ce1a9398fca6551
sherif-med <cherif.abedrazek@gmail.com>
2023-07-13 21:10:07 +0200

    adding pms file check at extraction && adding info_dict as return value && fxing minor file path issues

--- LxGeoPyLibs/satellites/extraction/airbus/one_atlas_extractor.py
+++ LxGeoPyLibs/satellites/extraction/airbus/one_atlas_extractor.py
@@ -36,23 +36,33 @@ def extract_single_zip(zip_file_path, extract_path):
     if not RAW_DATA_FOLDER.is_dir():
         os.makedirs(RAW_DATA_FOLDER)
     
+
+    info_dict = {
+        "RAW_DATA_FOLDER":RAW_DATA_FOLDER
+    }
+
+
     TIF_files = []
     xml_file = None
     opened_zipfile = ZipFile(zip_file_path, 'r')
     # Selecting files
     filenames = opened_zipfile.namelist()
     for filename in filenames:
-        filename=Path(filename)
-        if filename.suffix.lower() ==".tif":
+        if Path(filename).suffix.lower() ==".tif":
             TIF_files.append(filename)
         elif not xml_file and ("/dim" in str(filename).lower()) and (str(filename).lower().endswith("xml")):
             xml_file =filename
     
     # Extracting metdata xml
     if xml_file:
-        output_xml_path = RAW_DATA_FOLDER / f"imd{xml_file.suffix}"
+        output_xml_path = RAW_DATA_FOLDER / f"imd{Path(xml_file).suffix}"
         with open(str(output_xml_path.resolve()), 'wb') as out_f:
             out_f.write(opened_zipfile.read(str(xml_file)))
+        out_imd_file = os.path.join(RAW_DATA_FOLDER, "ortho.imd")
+        info_dict["XML"]=output_xml_path
+        with open( out_imd_file, "w" ) as imd_file:
+            iMetaData = IMetaData( str(output_xml_path) )
+            imd_file.write(iMetaData.IMD_geo())
     else:
         _logger.warning("Cannot find xml metadata in zipfile!")
 
@@ -61,44 +71,69 @@ def extract_single_zip(zip_file_path, extract_path):
     pan_parts_paths=[]
     ms_parts_count=len( list(filter(lambda x : "_MS_" in Path(x).stem, TIF_files) ))
     ms_parts_paths=[]
+    pms_parts_count =  len( list(filter(lambda x : "_PMS_" in Path(x).stem, TIF_files) ))
+    pms_parts_paths=[]
     
     # Extracting files tifs    
     for TIF_file in TIF_files:
-        tif_base_name = TIF_file.stem
+        tif_base_name = Path(TIF_file).stem
         tif_part_name = tif_base_name.split("_")[-1]
-        output_path = RAW_DATA_FOLDER / (f"_P_{tif_part_name}.tif" if "_P_" in tif_base_name else f"_MS_{tif_part_name}.tif")
+        if "_P_" in tif_base_name:
+            output_path = RAW_DATA_FOLDER / f"_P_{tif_part_name}.tif"
+            pan_parts_paths.append(output_path)
+        elif "_MS_" in tif_base_name:
+            output_path = RAW_DATA_FOLDER / f"_MS_{tif_part_name}.tif"
+            ms_parts_paths.append(output_path)
+        elif "_PMS_" in tif_base_name:
+            output_path = RAW_DATA_FOLDER / f"_PMS_{tif_part_name}.tif"
+            pms_parts_paths.append(output_path)
+        else:
+            raise Exception("Unkown tif file file representation!")
         with open(output_path, 'wb') as out_f:
             out_f.write(opened_zipfile.read(str(TIF_file)))
-        pan_parts_paths.append(output_path) if "_P_" in output_path.stem else ms_parts_paths.append(output_path)
+        
     
     ## Merging tif files parts
-    assert len(pan_parts_paths) == pan_parts_count
-    p_merged_output = RAW_DATA_FOLDER / "_P_.tif"
-    pan_parts_paths = list(map(lambda x:str(x) ,set(pan_parts_paths)))
-    if pan_parts_count > 0:
-        gm.main(['', '-o', str(p_merged_output)] + pan_parts_paths )
-        [os.remove(file) for file in pan_parts_paths]
-    else:
-        err_msg = "Wrong number of pan tif files!"
-        _logger.error(err_msg)
-        raise PartsMergeError(err_msg)
+    if pan_parts_count>0:
+        assert len(pan_parts_paths) == pan_parts_count
+        p_merged_output = RAW_DATA_FOLDER / "_P_.tif"
+        pan_parts_paths = list(map(lambda x:str(x) ,set(pan_parts_paths)))
+        if pan_parts_count > 0:
+            gm.main(['', '-o', str(p_merged_output)] + pan_parts_paths )
+            info_dict["P"]=p_merged_output
+            [os.remove(file) for file in pan_parts_paths]
+        else:
+            err_msg = "Wrong number of pan tif files!"
+            _logger.error(err_msg)
+            raise PartsMergeError(err_msg)
     
-    assert len(ms_parts_paths) == ms_parts_count
-    ms_merged_output = RAW_DATA_FOLDER / "_MS_.tif"
-    ms_parts_paths = list(map(lambda x:str(x) ,set(ms_parts_paths)))
-    if pan_parts_count > 0:
-        gm.main(['', '-o', str(ms_merged_output)] + ms_parts_paths )
-        [os.remove(file) for file in ms_parts_paths]
-    else:
-        err_msg = "Wrong number of ms tif files!"
-        _logger.error(err_msg)
-        raise PartsMergeError(err_msg)
-    
-    return {
-        "P":p_merged_output,
-        "MS":ms_merged_output,
-        "XML":output_xml_path
-    }
+    if ms_parts_count>0:
+        assert len(ms_parts_paths) == ms_parts_count
+        ms_merged_output = RAW_DATA_FOLDER / "_MS_.tif"
+        ms_parts_paths = list(map(lambda x:str(x) ,set(ms_parts_paths)))
+        if ms_parts_count > 0:
+            gm.main(['', '-o', str(ms_merged_output)] + ms_parts_paths )
+            info_dict["MS"]=ms_merged_output
+            [os.remove(file) for file in ms_parts_paths]
+        else:
+            err_msg = "Wrong number of ms tif files!"
+            _logger.error(err_msg)
+            raise PartsMergeError(err_msg)
+
+    if pms_parts_count>0:
+        assert len(pms_parts_paths) == pms_parts_count
+        pms_merged_output = RAW_DATA_FOLDER / "_PMS_.tif"
+        pms_parts_paths = list(map(lambda x:str(x) ,set(pms_parts_paths)))
+        if pms_parts_count > 0:
+            gm.main(['', '-o', str(pms_merged_output)] + pms_parts_paths )
+            info_dict["PMS"]=pms_merged_output
+            [os.remove(file) for file in pms_parts_paths]
+        else:
+            err_msg = "Wrong number of pms tif files!"
+            _logger.error(err_msg)
+            raise PartsMergeError(err_msg)
+
+    return info_dict
 
 
 @click.command()
@@ -118,12 +153,12 @@ def main(input_path, output_path, delete_dezipped, verbose):
         log_level = logging.INFO
     configure_logging(log_level=log_level)
     
-    is_zip_file_predicate = lambda x:x.lower().endswith(".zip")
+    is_zip_file_predicate = lambda x:str(x).lower().endswith(".zip")
     if input_path.is_dir():
-        c_zipfile_paths = filter(is_zip_file_predicate,
+        c_zipfile_paths = list(filter(is_zip_file_predicate,
         map(lambda x: input_path / x, os.listdir(input_path))
-        )
-        out_zipfile_paths = map(lambda x: x.stem / c_zipfile_paths)
+        ))
+        out_zipfile_paths = list(map(lambda x: output_path/x.stem, c_zipfile_paths))
     else:
         c_zipfile_paths = [input_path]
         out_zipfile_paths = [output_path]
@@ -137,29 +172,26 @@ def main(input_path, output_path, delete_dezipped, verbose):
             _logger.error(str(e))
             shutil.rmtree(outdir)
             continue
-
-        try:            
-            # pansharpen image
-            out_pansharpened_path = os.path.join(outdir, "ortho.tif")
-            exec_code = pansharpen_image(str(ext_dict["P"]), str(ext_dict["MS"]), out_pansharpened_path)
-            if exec_code == 1:
-                err_msg = f"Failed pansharpening to {out_pansharpened_path}"
-                raise PansharpeningError(err_msg)
-            
-            # pansharpen deletes all files with same basename as its output
-            out_imd_file = os.path.join(outdir, "ortho.imd")
-            with open( out_imd_file, "w" ) as imd_file:
-                iMetaData = IMetaData( str(ext_dict["XML"]) )
-                imd_file.write(iMetaData.IMD_geo())
-            
-        except Exception as e:
-            _logger.error(f"Failed postprocessing of extracted zipfile at: {outdir}")
-            _logger.error(str(e))
-            continue
+        
+        if not ext_dict.get("PMS"):
+            try:            
+                # pansharpen image
+                out_pansharpened_path = os.path.join(outdir, "ortho.tif")
+                exec_code = pansharpen_image(str(ext_dict["P"]), str(ext_dict["MS"]), out_pansharpened_path)
+                if exec_code == 1:
+                    err_msg = f"Failed pansharpening to {out_pansharpened_path}"
+                    raise PansharpeningError(err_msg)
+                
+                # pansharpen deletes all files with same basename as its output
+                
+            except Exception as e:
+                _logger.error(f"Failed postprocessing of extracted zipfile at: {outdir}")
+                _logger.error(str(e))
+                continue
 
         if delete_dezipped:
             try:
-                shutil.rmtree(os.path.dirname(ext_dict["P"]))
+                shutil.rmtree(ext_dict["RAW_DATA_FOLDER"])
             except Exception as e:
                 _logger.error("Error deleting dezipped folder!")
                 _logger.error(str(e))
