9323d09b1b253bfcaede0e58d0154a2fbd272d4d
sherif-med <cherif.abedrazek@gmail.com>
2023-07-13 20:48:57 +0200

    adding examples of meatadat computation using satellites metadata

--- LxGeoPyLibs/satellites/formulas.py
+++ LxGeoPyLibs/satellites/formulas.py
@@ -26,3 +26,23 @@ def nicolas_roof2roof(satAz1_r, satEl1_r, satAz2_r, satEl2_r):
 
 
 
+if __name__=="__main__":
+    from LxGeoPyLibs.satellites.imd import IMetaData
+    imd1_path = "C:/DATA_SANDBOX/Alignment_Project/PerfectGT/Pakistan_Rawalpindi_A_Neo/4d8738a2-816a-4924-b132-05fd48d011bb.IMD"
+    imd2_path = "C:/DATA_SANDBOX/Alignment_Project/PerfectGT/Pakistan_Rawalpindi_B_Neo/5347f144-8bd0-4b5f-9b33-6aa1e7b469ed.IMD"
+    imd1 = IMetaData(imd1_path)
+    imd2 = IMetaData(imd2_path)
+
+    dx1, dy1 = compute_roof2roof_constants(math.radians(imd1.satAzimuth()), math.radians(imd1.satElevation()),
+                                         math.radians(imd2.satAzimuth()), math.radians(imd2.satElevation()))
+    
+    dx2, dy2 = compute_roof2roof_constants(math.radians(imd2.satAzimuth()), math.radians(imd2.satElevation()),
+                                         math.radians(imd1.satAzimuth()), math.radians(imd1.satElevation()))
+    
+    rot_angle1 = compute_rotation_angle(math.radians(imd1.satAzimuth()), math.radians(imd1.satElevation()),
+                                         math.radians(imd2.satAzimuth()), math.radians(imd2.satElevation()))
+    
+    rot_angle2 = compute_rotation_angle(math.radians(imd2.satAzimuth()), math.radians(imd2.satElevation()),
+                                         math.radians(imd1.satAzimuth()), math.radians(imd1.satElevation()))
+
+    pass
