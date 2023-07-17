2abe24513485d048c917e67f0817e4a109f0776e
sherif-med <cherif.abedrazek@gmail.com>
2023-07-17 18:13:20 +0200

    adding common datasets && fixing typos

--- LxGeoPyLibs/dataset/common_interfaces.py
+++ LxGeoPyLibs/dataset/common_interfaces.py
@@ -1,37 +1,49 @@
-from LxGeoPyLibs.dataset.patchified_dataset import PatchifiedDataset
+from torch.utils.data import Dataset
+from functools import cached_property
 
 class SpatialyProjectedDataset(object):
     def __init__(self, crs):
         self._crs=crs
+    
+    @cached_property
     def crs(self):
         return self._crs
 
-class BoundedDataset(object):
+class BoundedDataset(SpatialyProjectedDataset):
 
-    def __init__(self, bounds_geom):
+    def __init__(self, bounds_geom, crs=None):
         self.bounds_geom = bounds_geom
+        SpatialyProjectedDataset.__init__(self, crs)
 
-class PixelizedDataset(PatchifiedDataset):
+class Pixelized2DDataset(object):
+    def __init__(self, x_pixel_size, y_pixel_size):
+        self.x_pixel_size=x_pixel_size
+        self.y_pixel_size=y_pixel_size
 
-    def __init__(self, pixel_x_size, pixel_y_size):
-        self.pixel_x_size=pixel_x_size
-        self.pixel_y_size=pixel_y_size
+class PreProcessedDataset(object):
+    
+    def __init__(self, parent_dataset ,preprocessing_callable):
+        self.parent_dataset=parent_dataset
+        self.preprocessing_callable = preprocessing_callable
+    
+    def __getitem__(self, index):
+        parent_item = self.parent_dataset.__getitem__(index)
+        return self.preprocessing_callable(parent_item)
 
-    ### should be fixed to meters not pixels
-    def setup_patch_per_pixel(self, pixel_patch_size, pixel_patch_overlap, bounds_geom):
-        """
-        Setup patch loading settings using spatial coordinates.
-        Args:
-            patch_size: a tuple of positive integers in pixels.
-            patch_overlap: a positive integer in pixels.
-            bounds_geom: pygeos polygon
-        """
-        self.pixel_patch_size= pixel_patch_size
-        self.pixel_patch_overlap= pixel_patch_overlap
-
-        patch_size_spatial = (self.pixel_patch_size[0]*self.pixel_x_size, self.pixel_patch_size[1]*self.pixel_y_size)
-        patch_overlap_spatial = self.pixel_patch_overlap*self.pixel_x_size
-
-        PatchifiedDataset.__init__(self, patch_size_spatial, patch_overlap_spatial, bounds_geom)
-
-
+class AugmentedDataset(object):
+    
+    def __init__(self, wrapped_dataset, augmentations):
+        self.wrapped_dataset = wrapped_dataset
+        self.augmentations = augmentations
+    
+    def __len__(self):
+        return len(self.wrapped_dataset) * len(self.augmentations)
+    
+    def __getitem__(self, idx):        
+        parent_item_idx = idx // (len(self.augmentations))
+        c_augmentation_idx = idx % (len(self.augmentations))
+        c_augmentation = self.augmentations[c_augmentation_idx]
+        
+        parent_item = self.wrapped_dataset[parent_item_idx]
+        return c_augmentation(parent_item)
+        
