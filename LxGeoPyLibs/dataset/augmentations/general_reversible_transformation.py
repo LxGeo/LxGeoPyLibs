464005986714e3899c2aaad1a975ddf45f56794b
sherif-med <cherif.abedrazek@gmail.com>
2023-08-22 01:25:14 +0200

    adding composed transformation base

--- LxGeoPyLibs/dataset/augmentations/general_reversible_transformation.py
+++ LxGeoPyLibs/dataset/augmentations/general_reversible_transformation.py
@@ -15,6 +15,23 @@ class GeneralIrrevirsibleTransformation:
     def __fwd__(self, *args):
         raise NotImplementedError
 
+class ComposedTransformation:
+    
+    def __init__(self, *sub_transforms):
+        self.sub_transforms=list(sub_transforms)
+    
+    def __fwd__(self, item, **kwargs):
+        out_item = item
+        for sub_t in self.sub_transforms:
+            out_item = sub_t(out_item, **kwargs)
+        return out_item
+    
+    def __rwd__(self, item, **kwargs):
+        out_item = item
+        for sub_t in self.sub_transforms[::-1]:
+            out_item = sub_t(out_item, **kwargs)
+        return out_item
+
 class GeneralRevrsibleTransformation:
     
     def __init__(self, fwdfn, rwdfn):
